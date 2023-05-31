05e162821de0dd100337e9d6dc7bdebe95004abb
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-31 21:53:29 +0900

    Fix Hilt varsion

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -2,12 +2,12 @@ package com.example.android.hilt.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.data.AppDatabase
 import com.example.android.hilt.data.LogDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
