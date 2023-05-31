15ba5936886c9910362782b17fb3929c53471fc6
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-05-31 09:23:06 +0900

    Add comment

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -9,11 +9,16 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @InstallIn(ApplicationComponent::class)
 @Module
 object DatabaseModule {
 
+    // @Providesを付けることで、プロジェクトにない別のモジュールのインスタンスの提供ができるようになる。
+    // AppDatabaseはRoomによって生成されるため、プロジェクトで定義されているものではない
+    @Provides
+    @Singleton
     fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
         return Room.databaseBuilder(
             appContext,
@@ -22,6 +27,7 @@ object DatabaseModule {
         ).build()
     }
 
+    // @Providesを付けることで、インターフェース等のコンストラクタを持たない型のインスタンスを提供できるようになる
     @Provides
     fun provideLogDao(database: AppDatabase): LogDao {
         return database.logDao()
