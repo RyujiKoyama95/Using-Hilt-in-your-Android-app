50166252242ab4d050c905194d69490bb2f5e4c5
Ryuji Koyama <ryujikoyama95@gmail.com>
2023-06-02 09:20:13 +0900

    Add LoggerInMemoryDataSource

--- app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
+++ app/src/main/java/com/example/android/hilt/data/LoggerInMemoryDataSource.kt
@@ -1,4 +1,19 @@
 package com.example.android.hilt.data
 
-class LoggerInMemoryDataSource {
+import java.util.LinkedList
+
+class LoggerInMemoryDataSource: LoggerDataSource {
+    private val logs = LinkedList<Log>()
+
+    override fun addLog(msg: String) {
+        logs.addFirst(Log(msg, System.currentTimeMillis()))
+    }
+
+    override fun getAllLogs(callback: (List<Log>) -> Unit) {
+        callback(logs)
+    }
+
+    override fun removeLogs() {
+        logs.clear()
+    }
 }
